77115ec64683b1465b8c0e3cc739451ea41ea582
Keith Lee <keithlee@Lee-MacBook-Air.local>
2022-11-09 15:51:00 -0500

    addded maps

--- script.R
+++ script.R
@@ -2,15 +2,11 @@
 library(tidyverse)
 library(scales)
 library(readxl)
+library(ggthemes)
 
-#Set theme
-
-theme_set(theme_classic())
 
 # load data
 qualtrics_data <- read_excel("qualtrics-data.xlsx")
-
-
 
 # restructure data
 
@@ -38,10 +34,10 @@ qualtrics_data %>%
   ggplot(aes(food_security, fill = food_security)) +
     geom_bar() +
     scale_fill_brewer(palette = "Dark2") +
-    theme_classic() +
+    theme_fivethirtyeight() +
     theme(legend.position = "none",
         plot.title = element_text(hjust = 0.5)) +
-    labs(title = "There is _______ food in my home to satisfy the needs of those in my household",
+    labs(title = "There is _______ food in my home to satisfy \n the needs of those in my household",
        x = "",
        y = "")
 
@@ -64,7 +60,7 @@ qualtrics_data %>%
     theme(legend.position = "right",
         plot.title = element_text(hjust = 0.5)) +
     labs(title = "Mean Income by Household Size",
-       x = "",
+       x = "Number of Residents",
        y = "")
 ggsave("income_by_housesize.png")
 
@@ -105,6 +101,7 @@ qualtrics_data$under18count[is.na(qualtrics_data$under18count)] <- 0
 ggplot(qualtrics_data, aes(under18count)) + 
   geom_histogram(color = "black",
                  fill = "gray") +
+  theme_classic() +
   labs(x = "Number of Dependents < 18 Years Old",
        y = "")
 
@@ -113,6 +110,21 @@ ggsave("under18count.png")
 qualtrics_data %>% 
   group_by(under18count) %>% 
   count() 
+
+# Bar chart
+
+qualtrics_data %>% 
+  drop_na(under18) %>% 
+  ggplot(aes(under18, fill = under18)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5)) +
+  labs(title = "",
+       x = "",
+       y = "")
+ggsave("under18.png")
 
 # housing
 
@@ -138,3 +150,114 @@ qualtrics_data %>%
 
 ggsave("housing.png")
 
+# Single Parent
+
+
+
+qualtrics_data$singleparent_f <- as.factor(qualtrics_data$singleparent)
+qualtrics_data$singleparent_f <- recode(qualtrics_data$singleparent_f,
+                                        "yes" = "Yes",
+                                        "NO" = "No",
+                                        "no" = "No",
+                                        "O" = "No",
+                                        "Widow" = "NA",
+                                        "N/a" = "NA",
+                                        "N/A" = "NA",
+                                        "Na" = "NA",
+                                        "1" = "NA",
+                                        "Just me  childen are all married" = "NA",
+                                        "My son moved in after , I lost my husband to covid." = "NA")
+qualtrics_data$singleparent_f <- na_if(qualtrics_data$singleparent_f,"NA")
+
+qualtrics_data %>% 
+  drop_na(singleparent_f) %>% 
+  ggplot(aes(singleparent_f, fill = singleparent_f)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5)) +
+  labs(title = "",
+       x = "",
+       y = "")
+ggsave("singleparent.png")
+
+# Over 65
+
+
+
+
+qualtrics_data$over65count_f <- recode(qualtrics_data$over65count,
+                                        "0" = "NA",
+                                        "myself" = "1",
+                                        "One" = "1",
+                                        "two" = "2")
+
+qualtrics_data$over65count_f <- na_if(qualtrics_data$over65count_f,"NA")
+
+                                        
+qualtrics_data$over65count_n <- as.numeric(qualtrics_data$over65count_f)
+
+
+# Run histogram
+
+qualtrics_data %>% 
+  drop_na(over65) %>% 
+  ggplot(aes(over65, fill = over65)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5)) +
+  labs(title = "",
+       x = "",
+       y = "")
+ggsave("over65.png")
+
+
+qualtrics_data_combined <- qualtrics_data %>% 
+  mutate(gfi = coalesce(gfi1, gfi2, gfi3, gfi4, gfi5, gfi6, gfi7, gfi8)) 
+
+
+# Employment Affected by COVID
+
+table(qualtrics_data$employ_affect_covid)
+
+# Temp Reduction in Hours
+
+table(qualtrics_data$temp_reduction_hours)
+
+
+# Permanent Reduction in Hours
+
+table(qualtrics_data$perm_reduction_hours)
+
+
+# Temporary Out of Work
+
+table(qualtrics_data$temp_out_of_work)
+
+
+
+# Permanent Out of Work
+
+table(qualtrics_data$perm_out_of_work)
+
+# Work Remotely
+
+table(qualtrics_data$work_remotely)
+
+# How Found Work
+
+table(qualtrics_data$how_found_work)
+
+# Require Child Care
+
+table(qualtrics_data$require_child_care)
+
+# Used Community Services
+
+table(qualtrics_data$used_community_nonprofit_services)
+
+table(qualtrics_data$how_found_work,
+      qualtrics_data$perm_out_of_work)
